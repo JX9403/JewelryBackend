a6820c0fdb788dc25ed214ec5827f3f7bd3fed3e
Doan Diem <diemdt9403@gmail.com>
2025-10-01 07:52:24 +0700

    init

---
+++ src/test/java/com/ndn/JewelryBackend/JewelryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndn.JewelryBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JewelryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
