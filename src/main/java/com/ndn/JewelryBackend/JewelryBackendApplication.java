4f73cc1fd62948f3c70720a212bc1871290538bb
Doan Diem <diemdt9403@gmail.com>
2025-10-01 07:52:24 +0700

    init

---
+++ src/main/java/com/ndn/JewelryBackend/JewelryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ndn.JewelryBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JewelryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JewelryBackendApplication.class, args);
+	}
+
+}
