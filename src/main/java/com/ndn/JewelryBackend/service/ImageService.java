9d293837b7dd80f53ec6ef52a5bb2766019a9983
Doan Diem <diemdt9403@gmail.com>
2025-10-14 01:15:39 +0700

    complete image, product, but not search/filter product, will do it later

--- src/main/java/com/ndn/JewelryBackend/service/ImageService.java
+++ src/main/java/com/ndn/JewelryBackend/service/ImageService.java
@@ -6,7 +6,6 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 public interface ImageService {
-    List<ImageResponse> uploadImages(Long productId, List<MultipartFile> files);
-    List<ImageResponse> getImagesByProduct(Long productId);
-    void deleteImage(Long id);
+    List<ImageResponse> uploadImages(List<MultipartFile> files);
+
 }
