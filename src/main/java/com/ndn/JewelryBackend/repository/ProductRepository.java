485ae998f2f21cf03e568f5ebac13b794803b7c1
Doan Diem <diemdt9403@gmail.com>
2025-11-27 21:29:04 +0700

    feat: update permit api for chat without authen

--- src/main/java/com/ndn/JewelryBackend/repository/ProductRepository.java
+++ src/main/java/com/ndn/JewelryBackend/repository/ProductRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
@@ -23,16 +22,12 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
             WHERE (:name IS NULL OR LOWER(p.name) LIKE LOWER(CONCAT('%', :name, '%')))
               AND (:categoryId IS NULL OR p.category.id = :categoryId)
               AND (:collectionId IS NULL OR p.collection.id = :collectionId)
-              AND (:priceFrom IS NULL OR p.price >= :priceFrom)
-              AND (:priceTo IS NULL OR p.price <= :priceTo)
               AND (:gender IS NULL OR p.gender = :gender)
             """)
     Page<Product> findAll(
             @Param("name") String name,
             @Param("categoryId") Long categoryId,
             @Param("collectionId") Long collectionId,
-            @Param("priceTo") BigDecimal priceTo,
-            @Param("priceFrom") BigDecimal priceFrom,
             @Param("gender") String gender,
             Pageable pageable
     );
